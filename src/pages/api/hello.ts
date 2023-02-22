9a2f4df16e9a174e2009e2481dbeefbf729a8f44
Haeseok Kang <hotbloodturtle@gmail.com>
2023-02-22 18:15:20 +0900

    :sparkles: add - prisma

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {prisma} from "@/lib/db";
 
 type Data = {
   name: string
+  users: any[]
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const users = await prisma.user.findMany()
+  res.status(200).json({name: 'John Doe', users})
 }
